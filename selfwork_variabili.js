6fe748b2e9cebe7134db3d1c1a62db2f525a3b9d
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2025-05-20 19:23:48 +0200

    caricato nello stesso selfwork delle variabili anche il selfwork degli operatori pt.1

--- selfwork_variabili.js
+++ selfwork_variabili.js
@@ -19,8 +19,35 @@ console.log(`Che cosa sono? ${stringa}`);
 
 
 const PIGRECO = `3.14159265359` ;
-PIGRECO = 30
+//PIGRECO = 30  
 console.log(`il valore di PIGRECO è  ${PIGRECO}`);
+
+
+// Selfwork Operatori 1
+let num1 = 10;
+console.log(`questo e il primo numero: ${num1}`);
+
+let num2 = 20;
+console.log(`questo e il primo numero: ${num2}`);
+
+let num3 = 30;
+console.log(`questo e il primo numero: ${num3}`);
+
+let num4 = 40;
+console.log(`questo e il primo numero: ${num4}`);
+
+let num5 = 50;
+console.log(`questo e il primo numero: ${num5}`);
+
+let somma2 = num1 + num2 + num3 + num4 + num5;
+
+console.log(`la somma dei numeri è: ${somma2}`);
+
+let media = somma2 / 5;
+
+console.log(`la media dei numeri è: ${media}`);
+
+
 
 
 
