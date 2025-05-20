a5d13c17c42505e471545a6fcc05165b45998b4f
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2025-05-20 15:46:32 +0200

    inizio

---
+++ selfwork_variabili.js
@@ -0,0 +1,28 @@
+let numero1 = 10;
+console.log(`questo e il primo numero: `+numero1);
+
+let numero2 = 20 
+console.log(`questo e il secondo numero: `+numero2);
+
+
+
+
+let somma = numero1 + numero2;
+
+console.log(`qusta e la somma del primo più il secondo numero: `+somma);
+
+let stringa = `Sono una stringa`;
+
+console.log(stringa);
+
+
+
+const PIGRECO = `3.14159265359` ;
+PIGRECO = 30
+console.log(`il valore di PIGRECO `+PIGRECO);
+
+
+
+
+
+
